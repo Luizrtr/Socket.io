8697780a459c934ba42e42f4b9ea7bb9ae49ce5d
Luiz <luiz.lr.2010@gmail.com>
2021-11-24 13:22:22 -0300

    add layout

--- client/src/chat/index.tsx
+++ client/src/chat/index.tsx
@@ -6,23 +6,117 @@ import { Container, Seach } from "./styles";
 export const Chat: React.FC = () => {
   return (
     <Container>
-      <ul tw="bg-black">
+      <ul>
         <Seach>
-          <input type="search" name="serch" placeholder="Search" />
-          <button type="submit">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              id="Capa_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 56.966 56.966"
-              width="512px"
-              height="512px"
-            >
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-            </svg>
-          </button>
+          <div tw="flex flex-col flex-auto h-full p-6">
+            <div tw="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
+              <div tw="flex flex-col h-full overflow-x-auto mb-4">
+                <div tw="grid grid-cols-12 gap-y-2">
+                  <div tw="col-start-1 col-end-8 p-3 rounded-lg">
+                    <div tw="flex flex-row items-center">
+                      <div tw="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                        A
+                      </div>
+                      <div tw="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                        <div>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Vel ipsa commodi illum saepe numquam maxime
+                          asperiores voluptate sit, minima perspiciatis.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div tw="col-start-6 col-end-13 p-3 rounded-lg">
+                    <div tw="flex items-center justify-start flex-row-reverse">
+                      <div tw="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                        A
+                      </div>
+                      <div tw="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                        <div>I'm ok what about you?</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div tw="col-start-6 col-end-13 p-3 rounded-lg">
+                    <div tw="flex items-center justify-start flex-row-reverse">
+                      <div tw="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                        A
+                      </div>
+                      <div tw="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                        <div>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div tw="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+                <div>
+                  <button tw="flex items-center justify-center text-gray-400 hover:text-gray-600">
+                    <svg
+                      tw="w-5 h-5"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+                <div tw="flex-grow ml-4">
+                  <div tw="relative w-full">
+                    <input
+                      type="text"
+                      tw="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                    />
+                    <button tw="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                      <svg
+                        tw="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div tw="ml-4">
+                  <button tw="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                    <span>Send</span>
+                    <span tw="ml-2">
+                      <svg
+                        tw="w-4 h-4 transform rotate-45 -mt-px"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                        ></path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </Seach>
       </ul>
     </Container>
--- client/src/chat/styles.ts
+++ client/src/chat/styles.ts
@@ -1,19 +1,5 @@
 import styled from "styled-components";
-import tw from "twin.macro";
 
 export const Container = styled.div``;
 
-export const Seach = styled.div`
-  ${tw`relative text-gray-600`}
-
-  input {
-    ${tw`bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none`}
-  }
-
-  button {
-    ${tw`absolute right-0 top-0 mt-3 mr-4`}
-    svg {
-      ${tw`h-4 w-4 fill-current`}
-    }
-  }
-`;
+export const Seach = styled.div``;
